4b30c82085b7ed5eb585cd47c4ccd7eb41fa0a8e
Gavin John <gavinnjohn@gmail.com>
2025-07-02 15:02:57 -0400

    It's modular, so it's good

--- flake.nix
+++ flake.nix
@@ -32,28 +32,115 @@
               ]
               (builtins.readFile "${inputs.genix7000}/to-image.nu")
           );
+        validateArgs =
+          args: argsType:
+          (lib.evalModules {
+            modules = [
+              {
+                options.args = lib.mkOption {
+                  type = argsType;
+                };
+              }
+              {
+                config.args = args;
+              }
+            ];
+          }).options.args.value;
+        mkIntTypeBetween =
+          start: end:
+          (lib.types.addCheck lib.types.int (x: x >= start && x <= end))
+          // {
+            description = "${lib.types.int.description} between ${toString start} and ${toString end}";
+          };
+        mkGenixFrameArgsType = lib.types.submodule {
+          options = {
+            numLambdas = lib.mkOption {
+              type = mkIntTypeBetween 3 25;
+              default = 6;
+              description = "Number of lambdas";
+            };
+            lambdaThickness = lib.mkOption {
+              type = mkIntTypeBetween 5 30;
+              default = 20;
+              description = "Lambda thickness (unknown units)";
+            };
+            imageWidth = lib.mkOption {
+              type = lib.types.int;
+              default = 256;
+              description = "Image width (in px)";
+            };
+            imageHeight = lib.mkOption {
+              type = lib.types.int;
+              default = 256;
+              description = "Image height (in px)";
+            };
+            offsetX = lib.mkOption {
+              type = lib.types.int;
+              default = -24;
+              description = "X offset of lambda (unknown units)";
+            };
+            offsetY = lib.mkOption {
+              type = lib.types.int;
+              default = -42;
+              description = "Y offset of lambda (unknown units)";
+            };
+            gapsX = lib.mkOption {
+              type = lib.types.int;
+              default = 3;
+              description = "X offset after clipping (use for gaps) (unknown units)";
+            };
+            gapsY = lib.mkOption {
+              type = lib.types.int;
+              default = -5;
+              description = "Y offset after clipping (use for gaps) (unknown units)";
+            };
+            rotation = lib.mkOption {
+              type = mkIntTypeBetween (-180) 180;
+              default = 0;
+              description = "Rotation of each lambda (in degrees)";
+            };
+            angle = lib.mkOption {
+              type = mkIntTypeBetween (-180) 180;
+              default = 30;
+              description = "Lambda arm angle (in degrees)";
+            };
+            clipRadius = lib.mkOption {
+              type = mkIntTypeBetween 0 300;
+              default = 92;
+              description = "Clipping n-gon radius (unknown units)";
+            };
+            clipRotation = lib.mkOption {
+              type = mkIntTypeBetween (-180) 180;
+              default = 0;
+              description = "Clipping n-gon rotation (in degrees)";
+            };
+            clipInverse = lib.mkOption {
+              type = lib.types.bool;
+              default = false;
+              description = "Reverse clipping order";
+            };
+            colors = lib.mkOption {
+              type = lib.types.listOf (
+                lib.types.addCheck lib.types.str (
+                  str:
+                  (lib.hasPrefix "#" str)
+                  && (lib.stringLength str == 7)
+                  && (lib.match "^[0-9a-fA-F]+$" (lib.substring 1 (lib.stringLength str - 1) str) != null)
+                )
+              );
+              default = [
+                "#5277C3"
+                "#7CAEDC"
+              ];
+              description = "Color palette to use";
+            };
+          };
+        };
         mkGenixFrame =
-          pkgs:
-          {
-            name,
-            numLambdas ? 6,
-            lambdaThickness ? 20,
-            imageWidth ? 256,
-            imageHeight ? 256,
-            offsetX ? -24,
-            offsetY ? -42,
-            gapsX ? 3,
-            gapsY ? -5,
-            rotation ? 0,
-            angle ? 30,
-            clipRadius ? 92,
-            clipRotation ? 0,
-            clipInverse ? false,
-            colors ? [
-              "#5277C3"
-              "#7CAEDC"
-            ],
-          }:
+          pkgs: name: rawArgs:
+          let
+            args = validateArgs rawArgs mkGenixFrameArgsType;
+          in
           pkgs.runCommand name
             {
               nativeBuildInputs = [
@@ -62,18 +149,18 @@
             }
             ''
               to-image \
-                --num ${toString numLambdas} \
-                --thick ${toString lambdaThickness} \
-                --imgsize "${toString imageWidth},${toString imageHeight}" \
-                --offset "${toString offsetX},${toString offsetY}" \
-                --gaps "${toString gapsX},${toString gapsY}" \
-                --rotation ${toString rotation} \
-                --angle ${toString angle} \
-                --clipr ${toString clipRadius} \
-                --cliprot ${toString clipRotation} \
-                --clipinv ${if clipInverse then "true" else "false"} \
+                --num ${toString args.numLambdas} \
+                --thick ${toString args.lambdaThickness} \
+                --imgsize "${toString args.imageWidth},${toString args.imageHeight}" \
+                --offset "${toString args.offsetX},${toString args.offsetY}" \
+                --gaps "${toString args.gapsX},${toString args.gapsY}" \
+                --rotation ${toString args.rotation} \
+                --angle ${toString args.angle} \
+                --clipr ${toString args.clipRadius} \
+                --cliprot ${toString args.clipRotation} \
+                --clipinv ${if args.clipInverse then "true" else "false"} \
                 ./tmp.png \
-                ${builtins.concatStringsSep " " (map (color: "\"${color}\"") colors)}
+                ${builtins.concatStringsSep " " (map (color: "\"${color}\"") args.colors)}
               mv ./tmp.png $out
             '';
         mkGenixPlymouthTheme =
@@ -92,9 +179,7 @@
               map (
                 frame:
                 "cp ${
-                  mkGenixFrame pkgs (
-                    (animation (frame / (frameRate + 0.0))) // { name = "${name}-frame-${toString frame}.png"; }
-                  )
+                  mkGenixFrame pkgs "${name}-frame-${toString frame}.png" (animation (frame / (frameRate + 0.0)))
                 } $out/share/plymouth/themes/${name}/frame-${toString frame}.png"
               ) (lib.range 0 (frameRate * duration - 1))
             ))
@@ -150,12 +235,12 @@
               ];
             };
             packages = {
-              testGenixFrame = mkGenixFrame pkgs { name = "test-genix-frame.png"; };
+              testGenixFrame = mkGenixFrame pkgs "test-genix-frame.png" { };
               testGenixPlymouthTheme = mkGenixPlymouthTheme pkgs {
                 name = "test-genix-plymouth-theme";
                 animation = time: {
                   # Wait WTF nix doesn't have ANY common math stuff?!
-                  lambdaThickness = builtins.floor (if time <= 1 then 20 + time * 100 else 20 + (2 - time) * 100);
+                  lambdaThickness = builtins.floor (if time <= 1 then 20 + time * 10 else 20 + (2 - time) * 10);
                   rotation = builtins.floor (if time <= 1 then time * 180 else (2 - time) * 180);
                 };
                 frameRate = 5;
@@ -168,126 +253,39 @@
             genix7000 =
               { config, pkgs, ... }:
               {
-                options =
-                  let
-                    argsType = lib.types.submodule {
-                      options = {
-                        numLambdas = lib.mkOption {
-                          type = lib.types.addCheck lib.types.int (x: x >= 3 && x <= 25);
-                          default = 6;
-                          description = "Number of lambdas";
+                options = {
+                  boot.plymouth.genix7000 = {
+                    enable = lib.mkEnableOption "automatically generated genix7000 boot animations";
+                    animation = lib.mkOption {
+                      type = lib.types.functionTo mkGenixFrameArgsType;
+                      example = ''
+                        time: {
+                          lambdaThickness = builtins.floor (if time <= 1 then 20 + time * 10 else 20 + (2 - time) * 10);
+                          rotation = builtins.floor (if time <= 1 then time * 180 else (2 - time) * 180);
                         };
-                        lambdaThickness = lib.mkOption {
-                          type = lib.types.int;
-                          default = lib.types.addCheck lib.types.int (x: x >= 5 && x <= 30);
-                          description = "Lambda thickness (unknown units)";
-                        };
-                        imageWidth = lib.mkOption {
-                          type = lib.types.int;
-                          default = 256;
-                          description = "Image width (in px)";
-                        };
-                        imageHeight = lib.mkOption {
-                          type = lib.types.int;
-                          default = 256;
-                          description = "Image height (in px)";
-                        };
-                        offsetX = lib.mkOption {
-                          type = lib.types.int;
-                          default = -24;
-                          description = "X offset of lambda (unknown units)";
-                        };
-                        offsetY = lib.mkOption {
-                          type = lib.types.int;
-                          default = -42;
-                          description = "Y offset of lambda (unknown units)";
-                        };
-                        gapsX = lib.mkOption {
-                          type = lib.types.int;
-                          default = -24;
-                          description = "X offset after clipping (use for gaps) (unknown units)";
-                        };
-                        gapsY = lib.mkOption {
-                          type = lib.types.int;
-                          default = -42;
-                          description = "Y offset after clipping (use for gaps) (unknown units)";
-                        };
-                        rotation = lib.mkOption {
-                          type = lib.types.addCheck lib.types.int (x: x >= -180 && x <= 180);
-                          default = 0;
-                          description = "Rotation of each lambda (in degrees)";
-                        };
-                        angle = lib.mkOption {
-                          type = lib.types.addCheck lib.types.int (x: x >= -180 && x <= 180);
-                          default = 30;
-                          description = "Lambda arm angle (in degrees)";
-                        };
-                        clipRadius = lib.mkOption {
-                          type = lib.types.addCheck lib.types.int (x: x >= 0 && x <= 300);
-                          default = 92;
-                          description = "Clipping n-gon radius (unknown units)";
-                        };
-                        clipRotation = lib.mkOption {
-                          type = lib.types.addCheck lib.types.int (x: x >= -180 && x <= 180);
-                          default = 0;
-                          description = "Clipping n-gon rotation (in degrees)";
-                        };
-                        clipInverse = lib.mkOption {
-                          type = lib.types.boolean;
-                          default = false;
-                          description = "Reverse clipping order";
-                        };
-                        colors = lib.mkOption {
-                          type = lib.types.listOf (
-                            lib.types.addCheck lib.types.str (
-                              str:
-                              (lib.hasPrefix "#" str)
-                              && (lib.stringLength str == 7)
-                              && (lib.match "^[0-9a-fA-F]+$" (lib.substring 1 (lib.stringLength str - 1) str) != null)
-                            )
-                          );
-                          default = [
-                            "#5277C3"
-                            "#7CAEDC"
-                          ];
-                          description = "Color palette to use";
-                        };
-                      };
+                      '';
+                      description = "A function that takes the frame time and returns a set of parameters to pass to to-image";
                     };
-                  in
-                  {
-                    boot.plymouth.genix7000 = {
-                      enable = lib.mkEnableOption "automatically generated genix7000 boot animations";
-                      animation = lib.mkOption {
-                        type = lib.types.functionTo argsType;
-                        example = ''
-                          time: {
-                            lambdaThickness = builtins.floor (if time <= 1 then 20 + time * 100 else 20 + (2 - time) * 100);
-                            rotation = builtins.floor (if time <= 1 then time * 180 else (2 - time) * 180);
-                          };
-                        '';
-                        description = "A function that takes the frame time and returns a set of parameters to pass to to-image";
+                    defaults = lib.mkOption {
+                      type = mkGenixFrameArgsType;
+                      example = {
+                        imageWidth = 64;
+                        imageHeight = 64;
                       };
-                      defaults = lib.mkOption {
-                        type = argsType;
-                        example = {
-                          imageWidth = 64;
-                          imageHeight = 64;
-                        };
-                        description = "a default set of parameters to pass to to-image";
-                      };
-                      frameRate = lib.mkOption {
-                        type = lib.types.int;
-                        default = 15;
-                        description = "The frame rate (in frames per second) of the animation";
-                      };
-                      duration = lib.mkOption {
-                        type = lib.types.int;
-                        example = 4;
-                        description = "The length (in seconds) of the animation";
-                      };
+                      description = "a default set of parameters to pass to to-image";
+                    };
+                    frameRate = lib.mkOption {
+                      type = lib.types.int;
+                      default = 15;
+                      description = "The frame rate (in frames per second) of the animation";
+                    };
+                    duration = lib.mkOption {
+                      type = lib.types.int;
+                      example = 4;
+                      description = "The length (in seconds) of the animation";
                     };
                   };
+                };
                 config =
                   let
                     cfg = config.boot.plymouth.genix7000;
